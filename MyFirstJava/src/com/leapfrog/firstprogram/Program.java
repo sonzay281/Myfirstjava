d285ee31a5ff88ef982cc0e0a81852ef0bd18a1d
Sanjaya Sapkota <sanjayasapkota54@gmail.com>
2016-06-20 13:56:59 +0545

    First Commit

---
+++ MyFirstJava/src/com/leapfrog/firstprogram/Program.java
@@ -0,0 +1,19 @@
+
+package com.leapfrog.firstprogram;
+
+/**
+ *
+ * @author Anonymous
+ */
+public class Program {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for(int i=0;i<=5;i++){
+       System.out.println(i+".Hello World");
+    }
+    
+}
+}
